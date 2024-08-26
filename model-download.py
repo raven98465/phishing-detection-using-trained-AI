a86def3a4448059420ddfdc3f524486a07b8c0eb
raven98465 <159140027+raven98465@users.noreply.github.com>
2024-08-26 10:16:17 +0800

    Add files via upload

--- model-download.py
+++ model-download.py
@@ -4,10 +4,11 @@ import os
 
 # Model Name List
 model_names = [
-#    'ealvaradob/bert-finetuned-phishing',
-#    'dima806/phishing-email-detection',
-#    'kamikaze20/phishing-email-detection_final_2'
-    'huynq3Cyradar/bert-large-finetuned-phishing-url-version'
+    'ealvaradob/bert-finetuned-phishing',
+    'dima806/phishing-email-detection',
+    'kamikaze20/phishing-email-detection_final_2',
+    'huynq3Cyradar/bert-large-finetuned-phishing-url-version',
+    'aibot123/phishing-detection-body-only'
 ]
 
 # Increase timeout and retry times
@@ -16,13 +17,16 @@ adapter = requests.adapters.HTTPAdapter(max_retries=5)
 session.mount("https://", adapter)
 timeout = (10, 30)  # Connection and read timeout
 
+# Retrieve the directory where the current script is located
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
 # Download and save models
 for model_name in model_names:
     tokenizer = AutoTokenizer.from_pretrained(model_name, use_auth_token=False)
     model = AutoModelForSequenceClassification.from_pretrained(model_name, use_auth_token=False)
 
-    # Obtain the local save path of the model
-    model_save_path = f'E:/PG-dissertation/AI model/{model_name.split("/")[-1]}'
+    # Dynamically save the path
+    model_save_path = os.path.join(script_dir, 'AI model', model_name.split("/")[-1])
 
     # Create a save path (if it does not exist)
     if not os.path.exists(model_save_path):
